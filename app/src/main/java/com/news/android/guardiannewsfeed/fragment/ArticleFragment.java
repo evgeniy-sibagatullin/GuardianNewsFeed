2124eb2eaa8f17ee3ba002f46954b698597262ce
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-06-30 19:32:24 +0300

    Provide forwarding to article data in browser.

--- app/src/main/java/com/news/android/guardiannewsfeed/fragment/ArticleFragment.java
+++ app/src/main/java/com/news/android/guardiannewsfeed/fragment/ArticleFragment.java
@@ -1,11 +1,14 @@
 package com.news.android.guardiannewsfeed.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.news.android.guardiannewsfeed.R;
@@ -20,12 +23,27 @@ public abstract class ArticleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.article_list_fragment, container, false);
-
         final List<Article> articles = getArticles();
+        final ArticleAdapter articleAdapter = new ArticleAdapter(getActivity(), articles);
 
-        ArticleAdapter articleAdapter = new ArticleAdapter(getActivity(), articles);
         ListView listView = (ListView) rootView.findViewById(R.id.article_list);
         listView.setAdapter(articleAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Article article = articleAdapter.getItem(position);
+
+                if (article != null) {
+                    String webUrl = article.getWebUrl();
+
+                    if (webUrl != null && !webUrl.isEmpty()) {
+                        Uri earthquakeUri = Uri.parse(webUrl);
+                        Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+                        startActivity(websiteIntent);
+                    }
+                }
+            }
+        });
 
         return rootView;
     }
